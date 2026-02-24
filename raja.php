f110b0c28fafbddd8f698cbe794f961cb37ae8c7
priyankasurapur-ui <priyankasurapur@gmail.com>
2026-02-24 10:33:59 +0530

    Add files via upload

---
+++ raja.php
@@ -0,0 +1,180 @@
+<html>
+<head>
+<title>
+RAJA HOSPITAL NAWANSHAHR
+</title>
+<style>
+body{
+background-color:white;
+color:black;
+text-align:justify;
+}
+.hospital {
+    width: 100%; /* Adjusted to 100% width */
+    display: flex;
+    flex-wrap: wrap; /* Added flex-wrap to wrap items */
+    justify-content: center; /* Center items horizontally */
+    gap: 20px; /* Added gap for spacing */
+	
+
+}
+
+.drs img:hover {
+    box-shadow: 5px 5px 20px black;
+}
+.drs1:hover{
+	color:darkblue;
+}
+.con{
+	font-size:25px;
+}
+.cont:hover{
+	color:darkblue;
+	
+}
+</style>
+<script>
+
+</script>
+</head>
+<body>
+
+<?php include 'menu.php'; ?>
+
+<table width="100%" height="150" bgcolor="lightblue"><tr><td align="center">
+<h1 align="center"><font size=" px";>RAJA HOSPITAL NAWANSHAHR </h1></table>
+
+<p><font size="5 px";>Raja Hospital is a prestigious NABH certified healthcare institution dedicated to delivering exceptional medical services with state-of-the-art facilities and infrastructure. Our mission is to establish a world-class integrated healthcare delivery system in North India, combining the highest level of medical expertise with compassionate patient care.</p>
+
+<table align="justify" width="100%"><tr align="right"><td>
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3415.4559718463443!2d76.11716637452258!3d31.12484937439746!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391abdc7cfb8f917%3A0xb0ca32d2ecda59d8!2sRaja%20Hospital%20%7C%20Nawanshahr%20(Punjab)!5e0!3m2!1sen!2sin!4v1721312270174!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></td>
+
+<td align="justify">
+<img src="raja.jpeg" class="raja.jpeg" width="600" height="400"></td>
+</table>
+
+<table border=0 bgcolor="darkblue" width="100%" height="400" style="border-radius: 15px;"><tr align="justify"><td align="justify" >
+<img src="vraja.jpeg" class="vraja.jpeg" width="600" height="400"></td>
+<td align="justify"><h1 align="center"><font color="white"><font size="6 px";>OUR VISION</h1><font color="white"><font size="5 px";>
+Our strategic vision is “To provide high quality health care services at affordable price through latest technology, equipment and human expertise in a caring and nurturing environment.”Hospital aims to be one of the most trusted hospitals in India. To achieve this, the hospital focuses on building strong relationships with its patients and their families. The hospital staff works closely with patients to understand their needs, concerns, and preferences, and to provide them with the highest standards of medical care. For example, the hospital’s doctors and nurses take the time to explain medical procedures, treatments, and medications to patients in a way that is easy to understand, which helps to build trust and confidence.</td></tr>
+</table><br>
+
+<table border=0 bgcolor="darkblue" width="100%" height="400" style="border-radius: 15px;"><tr align="justify" >
+<td align="justify"><h1 align="center"><font color="white"><font size="6 px";>OUR MISSION</h1><font color="white" ><font size="5 px";>
+We will set best practice standards in medical services continuously improving to exceed the expectations of our patients and families. We strongly believe in building and maintaining long term patient relationships, So as to become an essential resource for their well being.Research indicated that nearly all hospitals have adopted a management excellence approach and set goals within this framework to improve the performance of their health services organizations.The AAMC’s call to prioritize community collaboration and health equity as pillars of the academic medicine mission. </td>
+<td align="right">
+<img src="mraja.jpeg" class="mraja.jpeg" width="600" height="400"></td>
+</tr>
+</table>
+
+
+
+<h1 align="center"><font size="6 px";>OUR DOCTORS</h1>
+<p><font size="5 px";>At Raja Superspeciality Hospital we have assembled the finest talents in medicine, be they doctors, nurses or technicians, and even management professionals across a wide spectrum. Enabling them to deliver the highest quality of healthcare are state-of-the art facilities and support infrastructure at our Hospitals.</p>
+
+<table  border=0 bgcolor="white" width="100%" height="400"><tr align="justify" >
+           <td align="jusitify"><div class="hospital"><div class="drs">
+<img src="dra.jpeg" class="dra.jpeg" width="450" height="400"></td></div></div>
+<td align="center"><div class="drs">
+<img src="drb.jpeg" class="drb.jpeg" width="450" height="400"></td></div>
+<td align="right"><div class="drs">
+<img src="drc.jpeg" class="drc.jpeg" width="450" height="400"></td></tr></div>
+
+<tr align="justify"><td align="center"><div class="drs1"><font size="4 px";><strong>
+Dr. Jaswinder Singh<br>
+Founder & MD Raja Hospital (MBBS, DMRD)<br>
+Consultant Radiologist<br></td></strong></div>
+
+
+<td align="center"><div class="drs1"><font size=" px";><strong>
+Dr. Neelam Saini<br>
+Co-Founder and MS Raja Hospital (MBBS, Ex-PCMS)<br>
+Obstetric & Gynaecology</td></strong></div>
+
+<td align="center"><div class="drs1"><font size="4 px";><strong>
+Dr. Lakshita Saini<br>
+(MBBS, MS Obs & Gynae)<br>
+Fertility Expert<br>
+<br></td>
+</tr>
+</strong></div>
+
+<tr align="justify">
+           <td align="jusitify"><div class="hospital"><div class="drs">
+<img src="drd.jpeg" class="drd.jpeg" width="450" height="400"></td></div></div>
+<td align="center"><div class="drs">
+<img src="dre.jpeg" class="dre.jpeg" width="450" height="400"></td></div>
+<td align="right"><div class="drs">
+<img src="drf.jpeg" class="drf.jpeg" width="450" height="400"></td></tr></div>
+
+<tr align="justify"><td align="center"><div class="drs1"><font size="4 px";><strong>
+Dr. Amarinder Singh Saini<br>
+(MBBS, MD Radio Diagnosis)<br>
+Consultant Radiologist<br></td></strong></div>
+
+
+<td align="center"><div class="drs1"><font size="4 px";><strong>
+Dr. Arun K Gupta<br>
+(MBBS, MD Anaesthesia)<br>
+Consultant Anaesthesiologist, Intensivist and Pain Management Expert<br></td></strong></div>
+
+<td align="center"><div class="drs1"><font size="4 px";><strong>
+Dr. Piara Singh Bangar<br>
+(MD Anaesthesia)<br>
+Anaesthetist<br>
+<br></td>
+</tr>
+</strong></div>
+
+<tr align="justify">
+           <td align="jusitify"><div class="hospital"><div class="drs">
+<img src="drg.jpeg" class="drg.jpeg" width="450" height="400"></td></div></div>
+<td align="center"><div class="drs">
+<img src="drh.jpeg" class="drh.jpeg" width="450" height="400"></td></div>
+<td align="right"><div class="drs">
+<img src="dri.jpeg" class="dri.jpeg" width="450" height="400"></td></tr></div>
+
+<tr align="justify"><td align="center"><div class="drs1"><font size="4 px";><strong>
+Dr. Manjot Kaur<br>
+(MD Pathologist)<br>
+Gold Medalist<br></td></strong></div>
+
+
+<td align="center"><div class="drs1"><font size="4 px";><strong>
+Dr. Lakhwinder Singh <br>
+(MBBS, MS Orthopedics)<br>
+Joint Replacement and Arthroscopy Surgeon<br></td></strong></div>
+
+<td align="center"><div class="drs1"><font size="4 px";><strong>
+Dr. Raghav Vishesh Lumb<br>
+(MD Medicine) | (DM Cardiology)<br>
+Consultant Interventional Cardiologist<br></div>
+<br></td>
+</tr>
+</strong>
+
+</table>
+</div>
+
+<h1><font size="5 px";>OVERVIEW</h1>
+<p><font size="5 px";>Located in the heart of Nawanshahr City in the beautiful state of Punjab, India, Raja Hospital is a leading healthcare institution committed to providing exceptional medical care and services to the community. With 70 beds and a team of highly skilled medical professionals, we are dedicated to ensuring the well-being and recovery of our patients.
+
+At Raja Hospital, we pride ourselves on being a NABH (National Accreditation Board for Hospitals & Healthcare Providers) certified hospital, which reflects our unwavering commitment to maintaining the highest standards of quality, safety, and patient care. This accreditation demonstrates our continuous efforts to provide excellence in healthcare services.
+
+We understand the importance of state-of-the-art facilities in delivering accurate diagnoses and effective treatments. That’s why Raja Hospital is equipped with the best radiology labs in the region. Our advanced imaging technology enables our expert radiologists to provide precise and timely reports, aiding in the accurate diagnosis of various medical conditions.</p>
+
+<h1><font size="5 px";>ABOUT OUR SPEICIALITIES</h1>
+<p><font size="5 px";>As a trusted healthcare institution, Raja Hospital is renowned for its Trauma Centre, which provides round-the-clock emergency medical services. Our highly trained trauma team is skilled in handling critical situations and ensuring immediate and appropriate care for patients in need of urgent medical attention. We are dedicated to saving lives and providing comprehensive care during critical moments.
+
+Raja Hospital is proud to have been the first hospital in the city to introduce modern healthcare machines, revolutionizing the medical landscape of Nawanshahr. Our commitment to innovation ensures that our patients have access to the latest medical advancements, enabling accurate diagnoses, effective treatments, and enhanced patient outcomes.</p>
+ 
+<div class="con">
+<table border=0 bgcolor="lightblue" width="100%" height="200" align="center"><strong><tr align= "center"><th><div class="cont" ><font size = "6 px";>CONTACT US</th><br></tr></div>
+<tr align="center"><td><font color= "black"><div class="cont"><font size="5 px";>Mota Singh Road, Near Civil Surgeon Office, <br>Shahid Bhagat Singh Nagar, Nawanshahr, Punjab - 144514.<br>
+(+91) 8558899998<br>
+info@rajahospital.com <br>
+</td></tr></strong></table>
+</div>
+</body>
+</html>
