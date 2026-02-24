b0cac93f1963bdf7a259066a0f7897bae91e4b19
priyankasurapur-ui <priyankasurapur@gmail.com>
2026-02-24 10:32:22 +0530

    Add files via upload

---
+++ hospital.php
@@ -0,0 +1,162 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOSPITAL DIRECTORY NAWANSHAHR</title>
+    <style>
+	body {
+        background-color:white;
+		 background-image: url('wall.jpeg');
+	}
+        h1 {
+            font-family: 'Times New Roman', Times, serif;
+            text-align: center;
+			
+        }
+        p {
+            font-family: 'Times New Roman', Times, serif;
+            text-align: justify;
+        }
+       .routes {
+    width: 100%; /* Adjusted to 100% width */
+    display: flex;
+    flex-wrap: wrap; /* Added flex-wrap to wrap items */
+    justify-content: center; /* Center items horizontally */
+    gap: 20px; /* Added gap for spacing */
+}
+
+.oneroute img:hover {
+    box-shadow: 5px 5px 20px black;
+}
+
+.oneroute h3 {
+    font-size: 2vw;
+}
+#search {
+	color:red; 
+}
+#search {
+    border: 1px red;
+    border-radius: 100px;
+	font-size: 1.5vw;
+}
+
+#search:hover {
+    color: white;
+	align:center;
+	background-color:red;
+	box-shadow: 5px 5px 20px black;
+	font-family: 'Times New Roman', Times, serif;
+}
+
+.oneway h2:hover {
+    color: red;
+    cursor: pointer;
+}
+
+a:visited {
+    color: white;
+}
+		   
+    </style>
+    <script>
+        // Your JavaScript code (if any)
+    </script>
+</head>
+<body>
+<table border=0 width="100%" height="200" bgcolor="black" position="fixed"><tr><td align="center">
+    <h1 bgcolor="green;" width="100%";><font color="white">HOSPITAL DIRECTORY NAWANSHAHR</font></h1></td></tr>
+	</table>
+    <p style="background-color:white; border-radius:8px;"><font size="5px;">
+        Hospital, an institution that is built, staffed, and equipped for the diagnosis of disease; for the treatment, both medical and surgical, of the sick and the injured; and for their housing during this process. The modern hospital also often serves as a centre for investigation and for teaching.
+    
+        To better serve the wide-ranging needs of the community, the modern hospital has often developed outpatient facilities, as well as emergency, psychiatric, and rehabilitation services. In addition, “bedless hospitals” provide strictly ambulatory (outpatient) care and day surgery. Patients arrive at the facility for short appointments. They may also stay for treatment in surgical or medical units for part of a day or for a full day, after which they are discharged for follow-up by a primary care health provider.
+    </p>
+    
+    <table border=0 width="100%" height="200" bgcolor="black" position="fixed"><tr><td align="center"><h1 bgcolor="green;" width="100%";><font color="white">LIST OF HOSPITALS IN NAWANSHAHR</font></h1></td></tr>
+	</table>
+    <p style="background-color:white; border-radius:8px;"><font size="5px;">
+        Private hospitals offer a lot of advantages to their employees and patients, but some drawbacks come with these benefits. Some advantages of private hospitals include shorter wait times and more personalized care. However, one downside is that there is usually a higher cost associated with these benefits. A comparative study between public and private healthcare facilities found that many people preferred private hospitals because of the quality of care.
+    </p>
+	
+    <h4><font color="black" ><font size="5px;">
+        <ol type="1" style="background-color:white; border-radius:8px;">
+	
+            <li>RAJA HOSPITAL</li>
+            <li>A ONE HOSPITAL</li>
+            <li>DHAWAN HOSPITAL</li>
+            <li>HOPE HOSPITAL</li>
+            <li>APEX HOSPITAL</li>
+            <li>MAHI HOSPITAL</li>
+            <li>HI-TECH EYE HOSPITAL</li>
+            <li>SANDHU HOSPITAL</li>
+            <li>SARB HOSPITAL</li>
+            <li>SNS PAHWA HOSPITAL</li>
+        </ol>
+    </h4>
+	
+	 <table align="center"><tr align="center"><td align="center" ><div class="routes">
+            <div class="oneroute">
+                <img src="hoss.jpeg"><br>
+                <h1>RAJA HOSPITAL</h1>
+                <button id="search" onclick="search()"><a href="raja.php">Search </button><br></td>
+            </div>
+			
+			<td><div class="oneroute">
+                <img src="hoss.jpeg"><br>
+                <h1>A ONE HOSPITAL </h1>
+                <button id="search" onclick="search()"><a href="aone.php">Search </button><br></td>
+            </div>
+			
+           <td> <div class="oneroute">
+                <img src="hoss.jpeg"><br>
+                <h1>DHAWAN HOSPITAL</h1>
+                <button id="search" onclick="search()"><a href="dhawan.php">Search  </button><br></td>
+            </div>
+			
+          <tr align="center"> <td> <div class="oneroute">
+                <img src="hoss.jpeg"><br>
+                <h1>HOPE HOSPITAL</h1>
+                <button id="search" onclick="search()"><a href="hope.php">Search  </button><br></td>
+            </div>
+			
+           <td> <div class="oneroute">
+                <img src="hoss.jpeg"><br>
+                <h1>APEX HOSPITAL</h1>
+                <button id="search" onclick="search()"><a href="apex.php">Search </button><br></td>
+            </div>
+			
+           <td> <div class="oneroute">
+                <img src="hoss.jpeg"><br>
+                <h1>MAHI HOSPITAL </h1>
+               <button id="search" onclick="search()"><a href="mahi.php">Search  </button><br></td>
+            </div>
+			
+            <tr align="center"><td><div class="oneroute">
+                <img src="hoss.jpeg"><br>
+                <h1>HI-TECH EYE HOSPITAL</h1>
+                <button id="search" onclick="search()"><a href="hitech.php">Search  </button><br></td>
+            </div>
+			
+            <td><div class="oneroute">
+                <img src="hoss.jpeg"><br>
+                <h1>SANDHU HOSPITAL</h1>
+                <button id="search" onclick="search()"><a href="sandhu.php">Search  </button><br></td>
+            </div>
+			
+          <td> <div class="oneroute">
+                <img src="hoss.jpeg"><br>
+                <h1>SARB HOSPITAL </h5>
+                <button id="search" onclick="search()"><a href="sarb.php">Search  </button><br></td>
+            </div>
+			
+          <tr align="center"><td align="center"> <div class="oneroute">
+                <img src="hoss.jpeg"><br>
+                <h1>PAHWA HOSPITAL</h1>
+                <button id="search" onclick="search()"><a href="pahwa.php">Search  </button><br></td></tr></table>
+            </div>
+        </div>
+    </div>
+	
+</body>
+</html>
